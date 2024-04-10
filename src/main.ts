9ce7780eb7885de46f0153d7a1c211c1b67eb070
kevin-ledez <kevin.ledez@gmail.com>
2024-04-10 16:37:52 +0200

    👌 IMPROVE: Add background

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,21 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  function lines() {
+    let sizeW = Math.random() * 22;
+    let duration = Math.random() * 3;
+    let e = document.createElement("div");
+    e.setAttribute("class", "circle_background");
+    document.body.appendChild(e);
+    e.style.width = 12 + sizeW + "px";
+    e.style.left = Math.random() * +innerWidth + "px";
+    e.style.animationDuration = 2 + duration + "s";
+  
+    setTimeout(function () {
+      document.body.removeChild(e);
+    }, 5000);
+  }
+  setInterval(function () {
+    lines();
+  }, 200);
