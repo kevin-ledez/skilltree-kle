eebdd16d3f69448bef5062f79ed3a1f052a66c8d
Simon Vincent <simon.dev.vincent@gmail.com>
2024-04-12 10:22:26 +0200

    fix modale

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, HostListener } from '@angular/core';
 import { SupabaseService } from '../supabase.service';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatDialog, MatDialogConfig, MatDialogRef } from '@angular/material/dialog';
 import { ActivitesComponent } from '../activites/activites.component';
 import { CommonModule } from '@angular/common';
 
@@ -14,6 +14,7 @@ import { CommonModule } from '@angular/common';
 export class HomeComponent {
 
   utilisateurs: any;
+  dialogRef!: MatDialogRef<ActivitesComponent>; // Initialisation vide pour éviter l'erreur
 
   constructor(private supabase: SupabaseService, public dialog: MatDialog, private elementRef: ElementRef) {}
 
@@ -30,18 +31,18 @@ export class HomeComponent {
     dialogConfig.width = '100%';
     dialogConfig.data = { utilisateurId: utilisateurId };
 
-    // Ouvre la nouvelle fenêtre modale
-    const dialogRef = this.dialog.open(ActivitesComponent, dialogConfig);
+    // Ouvre la nouvelle fenêtre modale et stocke sa référence dans la variable dialogRef
+    this.dialogRef = this.dialog.open(ActivitesComponent, dialogConfig);
 
     // Ajouter un écouteur d'événements pour fermer la fenêtre modale en cliquant à l'extérieur
-    dialogRef.afterOpened().subscribe(() => {
+    this.dialogRef.afterOpened().subscribe(() => {
       this.elementRef.nativeElement.ownerDocument.addEventListener('click', this.dialogClickListener);
     });
   }
 
   // Fonction fléchée pour fermer la fenêtre modale lorsque l'utilisateur clique à l'extérieur
   dialogClickListener = (event: MouseEvent) => {
-    if (!this.elementRef.nativeElement.contains(event.target)) {
+    if (!this.elementRef.nativeElement.contains(event.target) && !this.dialogRef['_containerInstance']['_elementRef'].nativeElement.contains(event.target)) {
       this.dialog.closeAll();
       this.elementRef.nativeElement.ownerDocument.removeEventListener('click', this.dialogClickListener);
     }
